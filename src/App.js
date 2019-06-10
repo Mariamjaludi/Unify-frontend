e91ebb6e9fe7e76f1bdf6b0b57d7361425ffe04f
mariamjaludi <mariam.jaludi@gmail.com>
2019-06-10 15:57:57 +0100

    logic added to navbar for user signed in/signed out

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Search from './containers/SearchContainer'
-import Home from './containers/HomeContainer'
-import About from './containers/About'
-import Dashboard from './containers/Dashboard'
-import Navbar from './components/Navbar'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Search from "./containers/SearchContainer";
+import Home from "./containers/HomeContainer";
+import About from "./containers/About";
+import Dashboard from "./containers/Dashboard";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path='/about' component={About}/>
-          <Route path='/search' component={Search}/>
-          <Route path='/dashboard' component={Dashboard}/>
-        </Switch>
-      </div>
-    </Router>
-  );
+export default class App extends React.Component {
+  state = {
+    loggedIn: false
+  };
+
+  render() {
+    const { loggedIn } = this.state;
+    return (
+      <Router>
+        <div className="App">
+          <Navbar loggedIn={loggedIn} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/search" component={Search} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Menu } from 'semantic-ui-react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 export default class Navbar extends React.Component {
 
-  render () {
+  renderLogOutOrLogIn = loggedIn => {
+    if (loggedIn) {
+      return (
+        <Menu.Menu position="right">
+          <Menu.Item name="Welcome, Username" />
+          <Menu.Item name="log Out" />
+        </Menu.Menu>
+        )
+    } else {
+      return (
+        <Menu.Menu position="right">
+          <Menu.Item name="Sign Up" />;
+          <Menu.Item name="Log In" />;
+        </Menu.Menu>
+        )
+    }
+  };
+
+  render() {
+    const { loggedIn } = this.props;
     return (
       <Menu>
         <Menu.Item>
-          <Link to='/'>
-            Unify
-          </Link>
+          <Link to="/">Unify</Link>
         </Menu.Item>
         <Menu.Item>
-          <Link to='/about'>
-            About
-          </Link>
+          <Link to="/about">About</Link>
         </Menu.Item>
         <Menu.Item>
-          <Link to='/search'>
-            Search
-          </Link>
+          <Link to="/search">Search</Link>
         </Menu.Item>
         <Menu.Item>
-          <Link to='/dashboard'>
-            Dashboard
-          </Link>
+          <Link to="/dashboard">Dashboard</Link>
         </Menu.Item>
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='logout'
-          />
+        <Menu.Menu position="right">
+          {this.renderLogOutOrLogIn()}
         </Menu.Menu>
       </Menu>
-    )
+    );
   }
 }
